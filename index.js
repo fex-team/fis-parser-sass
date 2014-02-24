a41e18a50ba804d07f95741fdbe5657ddddc39ff
liaoxuezhi <liaoxuezhi@baidu.com>
2014-02-24 22:58:59 +0800

    replace node-sass with fis-sass, which support node 0.8.0+

--- index.js
+++ index.js
@@ -5,10 +5,11 @@
 
 'use strict';
 
-var sass = require('node-sass');
+var sass = require('fis-sass');
 
 module.exports = function(content, file, conf){
     var opts = fis.util.clone(conf);
     opts.data = content;
+    opts.include_paths = conf.include_paths || [ file.subdirname ];
     return sass.renderSync(opts);
 };
