723b7a2e54142ee2f18723f9cc041d836dede7e8
liaoxuezhi <liaoxuezhi@baidu.com>
2014-02-25 13:45:29 +0800

    update fis-sass from 0.1.4 to 0.1.6

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ var sass = require('fis-sass');
 module.exports = function(content, file, conf){
     var opts = fis.util.clone(conf);
     opts.data = content;
-    opts.include_paths = conf.include_paths || [ file.subdirname ];
+    opts.include_paths = conf.include_paths || [ file.dirname ];
+
     return sass.renderSync(opts);
 };
