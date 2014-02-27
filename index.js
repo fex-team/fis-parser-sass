1054734469578bddfee626aca2001a42f8f089ff
fouber <fouber.NET@gmail.com>
2014-02-27 09:53:39 +0800

    v0.0.5
    添加项目根路径到include_paths

--- index.js
+++ index.js
@@ -6,11 +6,11 @@
 'use strict';
 
 var sass = require('fis-sass');
+var root = fis.project.getProjectPath();
 
 module.exports = function(content, file, conf){
     var opts = fis.util.clone(conf);
     opts.data = content;
-    opts.include_paths = conf.include_paths || [ file.dirname ];
-
+    opts.include_paths = conf.include_paths || [ file.dirname, root ];
     return sass.renderSync(opts);
 };
