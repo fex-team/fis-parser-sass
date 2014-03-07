030419298d8fd2c4132dd5deec89e22ff1609a00
liaoxuezhi <liaoxuezhi@baidu.com>
2014-03-07 21:57:13 +0800

    Fix the encoding issue, and sperate the compiled binary files.

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ var root = fis.project.getProjectPath();
 module.exports = function(content, file, conf){
     var opts = fis.util.clone(conf);
     opts.data = content;
-    opts.include_paths = conf.include_paths || [ file.dirname, root ];
+    opts.include_paths = conf.include_paths || [];
+    opts.include_paths = opts.include_paths.concat([ file.dirname, root ]);
     return sass.renderSync(opts);
 };
